322263724ac3a64112246e199f89cf2d5d8d4161
Antão Almada <antao.almada@datasonar.io>
2018-03-13 09:30:40 +0000

    A couple more raw enumeration benchmarks

--- SpanSample/EnumerationBenchmarks.cs
+++ SpanSample/EnumerationBenchmarks.cs
@@ -16,7 +16,7 @@ namespace SpanSample
         {
             stream = new MemoryStream();
             var buffer = new byte[8];
-            for (var counter = 0; counter < 1_000_000; counter++)
+            for (var counter = 0; counter < 10_000_000; counter++)
             {
                 BitConverter.TryWriteBytes(buffer, (long)counter);
                 stream.Write(buffer);
@@ -32,7 +32,7 @@ namespace SpanSample
         }
 
         [Benchmark]
-        public long Sum_Raw()
+        public long Sum_RawForeach()
         {
             stream.Seek(0, SeekOrigin.Begin);
 
@@ -53,8 +53,54 @@ namespace SpanSample
             return sum;
         }
 
+        [Benchmark]
+        public long Sum_RawEnumerator()
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+
+            var itemSize = Marshal.SizeOf<Foo>();
+
+            Span<Foo> buffer = new Foo[itemsBufferCount]; // alloc items buffer
+            var rawBuffer = buffer.NonPortableCast<Foo, byte>(); // cast items buffer to bytes buffer (no copies)
+
+            var bytesRead = stream.Read(rawBuffer);
+            var sum = 0L;
+            while (bytesRead > 0)
+            {
+                var itemsRead = bytesRead / itemSize;
+                var enumerator = buffer.Slice(0, itemsRead).GetEnumerator();
+                while(enumerator.MoveNext()) // iterate through the item buffer
+                    sum += enumerator.Current.Integer; // Current property return a reference (no copy)
+                bytesRead = stream.Read(rawBuffer);
+            }
+            return sum;
+        }
+
         [Benchmark(Baseline = true)]
-        public long Sum_RawStack()
+        public long Sum_RawSquareBrackets()
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+
+            var itemSize = Marshal.SizeOf<Foo>();
+
+            Span<Foo> buffer = new Foo[itemsBufferCount]; // alloc items buffer
+            var rawBuffer = buffer.NonPortableCast<Foo, byte>(); // cast items buffer to bytes buffer (no copies)
+
+            var bytesRead = stream.Read(rawBuffer);
+            var sum = 0L;
+            while (bytesRead > 0)
+            {
+                var itemsRead = bytesRead / itemSize;
+                var values = buffer.Slice(0, itemsRead);
+                for (var index = 0; index < itemsRead; index++) // iterate through the item buffer
+                    sum += values[index].Integer; // square-brackets operator returns a reference (no copy)
+                bytesRead = stream.Read(rawBuffer);
+            }
+            return sum;
+        }
+
+        [Benchmark]
+        public long Sum_RawStackalloc()
         {
             stream.Seek(0, SeekOrigin.Begin);
 
@@ -68,8 +114,9 @@ namespace SpanSample
             while (bytesRead > 0)
             {
                 var itemsRead = bytesRead / itemSize;
-                foreach (var foo in buffer.Slice(0, itemsRead)) // iterate through the item buffer
-                    sum += foo.Integer;
+                var values = buffer.Slice(0, itemsRead);
+                for (var index = 0; index < itemsRead; index++) // iterate through the item buffer
+                    sum += values[index].Integer;
                 bytesRead = stream.Read(rawBuffer);
             }
             return sum;
